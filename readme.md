69949d310dd5c40c945d83c4cebd98c1ca93d7c9
keshav verma <keshavcode1@gmail.com>
2026-06-16 16:55:13 +0530

    counter app js

---
+++ readme.md
@@ -0,0 +1,4 @@
+simple html css js 
+counter app 
+
+used event listner in button 
---
+++ script.js
@@ -0,0 +1,22 @@
+let decbtn = document.getElementById("decrease")
+let resetbtn = document.getElementById("reset")
+let incbtn = document.getElementById("increase")
+let output = document.getElementById("output")
+let cnt =0;
+decbtn.addEventListener('click',()=>{
+    cnt--;
+    console.log(cnt);
+    output.innerHTML=`${cnt}`   
+});
+
+incbtn.addEventListener('click',()=>{
+    cnt++;
+    output.innerHTML=`${cnt}`
+
+})
+resetbtn.addEventListener('click',()=>{
+    cnt=0;
+    output.innerHTML=`${cnt}`
+})
+
+
